637ff0cf996d3235219e05640e4320f73557b122
victorcdrdm <victorcrombez@hotmail.com>
2020-11-30 16:19:55 +0100

    corection du js

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,8 @@
 console.log("hello world !")
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
+const $ = require('jquery');
+require('bootstrap');
 
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
